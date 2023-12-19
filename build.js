88286bb3e16f27b21886560af936603361bf0e4c
Marko Krstic <mailme@markokrstic.com>
2023-12-19 20:23:33 +0100

    ....

---
+++ build.js
@@ -0,0 +1,17 @@
+const esbuild = require("esbuild");
+
+async function build() {
+    try {
+        await esbuild.build({
+            entryPoints: ["src/main.js"],
+            bundle: true,
+            // minify: true,
+            outfile: "dist/main.min.js",
+        });
+        console.log("Build successful");
+    } catch (error) {
+        console.error("Error during build:", error);
+    }
+}
+
+build();
